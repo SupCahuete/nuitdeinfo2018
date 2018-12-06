7e44b7c9295ccab7eb243781d08d808231f1f08c
Guillaume GRACIET <guillaume@hexago-on.com>
2018-12-06 22:40:17 +0100

    Mon premier miaule.

--- resources/views/lang-dev/guest/welcome/index.blade.php
+++ resources/views/lang-dev/guest/welcome/index.blade.php
@@ -30,69 +30,6 @@
 
 @section('style')
   <link rel="stylesheet" href="@css('guest/welcome/index.css')">
-  <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-  <style>
-    html, body {
-      background-color: #fff;
-      color: #636b6f;
-      font-family: 'Raleway', sans-serif;
-      font-weight: 100;
-      height: 100vh;
-      margin: 0;
-    }
-
-    .full-height {
-      height: 100vh;
-    }
-
-    .flex-center {
-      align-items: center;
-      display: flex;
-      justify-content: center;
-    }
-
-    .position-ref {
-      position: relative;
-    }
-
-    .top-right {
-      position: absolute;
-      right: 10px;
-      top: 18px;
-    }
-
-    .content {
-      text-align: center;
-    }
-
-    .title {
-      font-size: 95px;
-    }
-
-    .title .title-mini {
-      display: block;
-      font-size: 20px;
-    }
-
-    .title .title-little {
-      display: block;
-      font-size: 50px;
-    }
-
-    .links > a {
-      color: #636b6f;
-      padding: 0 25px;
-      font-size: 12px;
-      font-weight: 600;
-      letter-spacing: .1rem;
-      text-decoration: none;
-      text-transform: uppercase;
-    }
-
-    .m-b-md {
-      margin-bottom: 70px;
-    }
-  </style>
 @endsection
 
 @section('script-head')
@@ -118,23 +55,7 @@
                     Content page core
 --------------------------------------------------------}}
 @section("content")
-  <div class="flex-center position-ref full-height">
-    <div class="content">
-      <div class="title m-b-md">
-        Laravel ADA
-        <br>
-        <span class="title-mini">By</span>
-        <span class="title-little">HexaGo'On</span>
-      </div>
-
-      <div class="links">
-        {{--<a href="">Documentation</a>--}}
-        <a href="https://github.com/hexago-on/ada">GitHub</a>
-        <a href="https://www.facebook.com/hexagoonCM/">Facebook</a>
-        <a href="https://twitter.com/hexago_on">Twitter</a>
-      </div>
-    </div>
-  </div>
+  <h1 class="red center">TEST</h1>
 @endsection
 
 
