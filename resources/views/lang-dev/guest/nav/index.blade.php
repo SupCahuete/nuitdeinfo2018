997342e1503ab0ba088204360f5c18b17534dc14
Eric <eric@hexago-on.com>
2018-12-07 06:07:00 +0100

    Mon second se dit d'un produit aillant passé sa data de consommation

---
+++ resources/views/lang-dev/guest/nav/index.blade.php
@@ -0,0 +1,121 @@
+
+{{--------------------------------------------------------
+                   Extends Layout Blade
+--------------------------------------------------------}}
+@extends('guest.layouts.main')
+
+
+
+
+{{--------------------------------------------------------
+                           Head
+--------------------------------------------------------}}
+@section('meta')
+  <meta name="description" content="">
+@endsection
+
+@section('favicon')
+@endsection
+
+@section('facebook')
+@endsection
+
+@section('twitter')
+@endsection
+
+@section('linkedin')
+@endsection
+
+@section('google')
+@endsection
+
+@section('style')
+  <link rel="stylesheet" href="@css('guest/resources/index.css')">
+@endsection
+
+@section('script-head')
+@endsection
+
+@section('title')
+  @config('app.name') - Navigation
+@endsection
+
+
+
+
+{{--------------------------------------------------------
+                      Alert messages
+--------------------------------------------------------}}
+@section("alert")
+@endsection
+
+
+
+
+{{--------------------------------------------------------
+                    Content page core
+--------------------------------------------------------}}
+@section("content")
+  <div id="map" style="height:795px;">
+
+  </div>
+@endsection
+
+
+
+
+{{--------------------------------------------------------
+                          Script
+--------------------------------------------------------}}
+@section('script-body')
+  <script type="text/javascript">
+    // Note: This example requires that you consent to location sharing when
+    // prompted by your browser. If you see the error "The Geolocation service
+    // failed.", it means you probably did not give permission for the browser to
+    // locate you.
+    var map, infoWindow;
+    function initMap() {
+      map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: -24.751163662, lng: 15.27249891},
+        zoom: 6
+      });
+
+      var marker = new google.maps.Marker({
+        position: {lat: -24.751163662, lng: 15.27249891},
+        map: map,
+        title: 'You are here!'
+      });
+
+      // infoWindow = new google.maps.InfoWindow;
+
+      // Try HTML5 geolocation.
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+
+          infoWindow.setPosition(pos);
+          infoWindow.setContent('Location found.');
+          infoWindow.open(map);
+          map.setCenter(pos);
+        }, function() {
+          handleLocationError(true, infoWindow, map.getCenter());
+        });
+      } else {
+        // Browser doesn't support Geolocation
+        handleLocationError(false, infoWindow, map.getCenter());
+      }
+    }
+
+    function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+      infoWindow.setPosition(pos);
+      infoWindow.setContent(browserHasGeolocation ?
+        'Error: The Geolocation service failed.' :
+        'Error: Your browser doesn\'t support geolocation.');
+      infoWindow.open(map);
+    }
+  </script>
+  <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBA5Hi1I2nQZBL_9xd_O3_rJsKJ9jAEt8k&callback=initMap"></script>
+@endsection
