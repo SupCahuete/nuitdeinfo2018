a7276ba6b0eefc674995db744ccdd78252c67b4a
mehdi <mehdi@hexago-on.com>
2018-12-07 07:55:50 +0100

    Un dernier pour la route.

--- resources/views/lang-dev/guest/layouts/standby.blade.php
+++ resources/views/lang-dev/guest/layouts/standby.blade.php
@@ -2,7 +2,5 @@
     <h2 class="green-text text-darken-4 center"><span class="date"></span> <span class="time"></span></h2>
     <p class="green-text text-darken-4 center">Economie d'énergie</p>
     <div class="separator"></div>
-    <h5 class="green-text text-darken-4 center"><i class="material-icons small">warning</i> Alertes:</h5>
-    <p class="green-text text-darken-4 center">[TELEMETRIE] QrX16 (Mule): Vérifier press. pneu av. g.</p>
-    <p class="green-text text-darken-4 center">[RESSOURCES] Pièces mécaniques: plus de led</p>
+    <h5 class="green-text text-darken-4 center"><i class="material-icons small">check</i>Appuyez pour déverouiller l'appareil</h5>
 </div>
